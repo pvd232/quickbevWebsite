2c91164b067cdd3b49081c449da41caa5e940d69
pvd232 <patardriscoll@gmail.com>
2021-03-06 23:25:23 -0500

    ugh

--- main.py
+++ main.py
@@ -66,6 +66,11 @@ def my_index():
     return render_template("index.html", flask_token="Hello world")
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
+
+
 @app.route("/b")
 def b():
     # test_service = Test_Service()
