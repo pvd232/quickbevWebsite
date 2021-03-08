a45287de4c3ecc2cc5d209e73dca841f817c29f8
pvd232 <patardriscoll@gmail.com>
2021-03-08 09:58:15 -0500

    adding changes to email verification

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ stripe.api_key = "sk_test_51I0xFxFseFjpsgWvh9b1munh6nIea6f5Z8bYlIDfmKyNq6zzrgg8i
 secret = '3327aa0ee1f61998369e815c17b1dc5eaf7e728bca14f6fe557af366ee6e20f9'
 # theme color RGB = rgb(134,130,230), hex = #8682E6
 # TODO: need to finish the add_business function by adding the new business address and returning the unique identifier to main.py so i can dynamically set the files path of the new image using the UUID of the business address
+# TODO: copy over changes from backendquickbev to website and push website, then finish customers and orders page
 
 
 def send_apn(device_token, action):
