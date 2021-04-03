0eb2629745aa9184044f5cedc6bc5d04b29bdf31
Peter <patardriscoll@gmail.com>
2021-04-03 17:16:12 -0400

    forgot to change api base url to be nothing

--- react-app/src/helpers/Api.js
+++ react-app/src/helpers/Api.js
@@ -2,7 +2,7 @@ import { Merchant } from "../Models.js";
 
 class Client {
   constructor() {
-    this.baseUrl = "http://127.0.0.1:8080";
+    this.baseUrl = "";
     this.url = "";
   }
   async makeRequest(
