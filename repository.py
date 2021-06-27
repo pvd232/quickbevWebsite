118c9ba8cd3fbc74ebd972d6ed0c5cf2e56d6d93
pvd232 <patardriscoll@gmail.com>
2021-06-27 00:36:01 -0400

    has_registered still throwing error but think i fixed by returning the newly created customer

--- repository.py
+++ repository.py
@@ -197,7 +197,7 @@ class Customer_Repository(object):
             new_customer = Customer(id=customer.id, password=customer.password,
                                     first_name=customer.first_name, last_name=customer.last_name, stripe_id=test_stripe_id.id, email_verified=customer.email_verified, has_registered=False)
             session.add(new_customer)
-            return customer
+            return new_customer
         elif not test_customer and not test_stripe_id:
             new_customer = stripe.Customer.create()
             new_stripe = Stripe_Customer(id=new_customer.id)
@@ -205,7 +205,7 @@ class Customer_Repository(object):
             new_customer = Customer(id=customer.id, password=customer.password,
                                     first_name=customer.first_name, last_name=customer.last_name, stripe_id=new_stripe.id, email_verified=customer.email_verified, has_registered=False)
             session.add(new_customer)
-            return customer
+            return new_customer
         # if the customer that has been requested for registration from the front end is unverified then we overwrite the customer values with the new values and return True to let the front end know that this customer has previously attempted to have been registered but was never verified. that way if a customer never verfies the account can continue to be modified as necessary while still preserving its unverified state
         elif test_customer and test_customer.email_verified == False and not test_stripe_id:
             new_customer = stripe.Customer.create()
