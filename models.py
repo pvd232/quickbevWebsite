ce629038b08175a3f4487140cad8f4abcc6ea20b
pvd232 <patardriscoll@gmail.com>
2021-10-29 17:13:10 -0700

    final push

--- models.py
+++ models.py
@@ -657,9 +657,9 @@ def create_etag():
 def create_everything():
     db.drop_all()
     db.create_all()
-    create_business()
-    create_drink()
-    create_orders_and_customers()
+    # create_business()
+    # create_drink()
+    # create_orders_and_customers()
     create_etag()
 
 
