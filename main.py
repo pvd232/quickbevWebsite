e052a2563b843d475fb6244f97b2daeaae7cd5f1
pvd232 <patardriscoll@gmail.com>
2021-11-24 21:56:39 -0500

    update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.config['UPLOAD_FOLDER'] = merchant_menu_upload_folder
 stripe.api_key = "sk_test_51I0xFxFseFjpsgWvh9b1munh6nIea6f5Z8bYlIDfmKyNq6zzrgg8iqeKEHwmRi5PqIelVkx4XWcYHAYc1omtD7wz00JiwbEKzj"
 secret = '3327aa0ee1f61998369e815c17b1dc5eaf7e728bca14f6fe557af366ee6e20f9'
 ip_address = "192.168.1.192"
-env = "local"
+env = "production"
 apns = "debug"
 # theme color RGB = rgb(134,130,230), hex = #8682E6
 # nice seafoam color #19cca3
@@ -99,15 +99,15 @@ def send_fcm(device_token, new_order):
     # client.send([registration_id], alert, **options)
 
 
-# @app.route("/")
-# def my_index():
+@app.route("/")
+def my_index():
 
-#     return render_template("index.html", flask_token="Hello world")
+    return render_template("index.html", flask_token="Hello world")
 
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return render_template('index.html')
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
 
 
 @app.route("/b")
