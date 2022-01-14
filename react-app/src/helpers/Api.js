b7fd341545bb4f16968813c04a078a07abd610e3
Peter Driscoll <patardriscoll@gmail.com>
2022-01-14 00:18:13 -0500

    pre-austin push

--- react-app/src/helpers/Api.js
+++ react-app/src/helpers/Api.js
@@ -2,7 +2,7 @@ import { Merchant, LocalStorageManager } from "../Models.js";
 
 class Client {
   constructor() {
-    this.baseUrl = "https://quickbev.usß";
+    this.baseUrl = "https://quickbev.us";
     // this.mode = "cors";
   }
   async makeRequest(
